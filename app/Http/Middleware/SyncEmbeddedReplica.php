94992a5070d860b9f24f3d4852b0480e602540a8
Richan Fongdasen <richan.fongdasen@gmail.com>
2024-11-07 00:29:45 +0700

    Apply some package updates

--- app/Http/Middleware/SyncEmbeddedReplica.php
+++ app/Http/Middleware/SyncEmbeddedReplica.php
@@ -4,7 +4,6 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 use RichanFongdasen\Turso\Facades\Turso;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -19,13 +18,7 @@ class SyncEmbeddedReplica
     {
         $response = $next($request);
 
-        if (
-            (config('database.default') === 'turso') &&
-            ((string) config('database.connections.turso.db_replica') !== '') &&
-            DB::hasModifiedRecords()
-        ) {
-            Turso::sync();
-        }
+        Turso::sync();
 
         return $response;
     }
