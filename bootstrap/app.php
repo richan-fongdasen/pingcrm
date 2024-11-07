45077b12e7fde888cf9cfff4a8365ce72d7486b1
Richan Fongdasen <richan.fongdasen@gmail.com>
2024-11-07 12:40:28 +0700

    Dropping SyncEmbeddedReplica middleware because we don't need it anymore

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Middleware\SyncEmbeddedReplica;
 use App\Providers\AppServiceProvider;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
@@ -9,14 +8,14 @@ use Illuminate\Foundation\Configuration\Middleware;
 return Application::configure(basePath: dirname(__DIR__))
     ->withProviders()
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
+        web: __DIR__ . '/../routes/web.php',
         // api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
+        commands: __DIR__ . '/../routes/console.php',
         // channels: __DIR__.'/../routes/channels.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->redirectGuestsTo(fn () => route('login'));
+        $middleware->redirectGuestsTo(fn() => route('login'));
         $middleware->redirectUsersTo(AppServiceProvider::HOME);
 
         $middleware->web(\App\Http\Middleware\HandleInertiaRequests::class);
@@ -24,8 +23,6 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->throttleApi();
 
         $middleware->replace(\Illuminate\Http\Middleware\TrustProxies::class, \App\Http\Middleware\TrustProxies::class);
-
-        $middleware->append(SyncEmbeddedReplica::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
